08cc5f962b0e09afd11985a46ab3530692535253
lihel <835943102@qq.com>
2021-06-25 16:40:07 +0800

    feat: eslint

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,6 +13,19 @@ export default defineComponent({
   },
   methods: {
     handleFun(name: String) {
+      interface Person {
+      readonly id: number;
+    name: string;
+    age?: number;
+    [propName: string]: any;
+  }
+
+let tom: Person = {
+    id: 89757,
+    name: 'Tom',
+    gender: 'male'
+};
+console.log(tom)
       return `hello, ${name}`;
     }
   }
